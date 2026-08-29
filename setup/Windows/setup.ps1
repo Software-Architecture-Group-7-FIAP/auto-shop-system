8da9d8c07960746b654462e82e36a9f9f854d124
Guilherme Bracero <guibragon@gmail.com>
2026-08-29 13:58:34 -0300

    feat: setup script added to facilitate the app execution

---
+++ setup/Windows/setup.ps1
@@ -0,0 +1,13 @@
+Write-Host "Iniciando Auto Shop..."
+
+docker build -q -t auto-shop-system:latest . | Out-Null
+
+kubectl apply -f k8s/ | Out-Null
+kubectl apply -f k8s/services | Out-Null
+
+Start-Process powershell -WindowStyle Hidden -ArgumentList "kubectl port-forward -n ingress-nginx service/ingress-nginx-controller 8080:80"
+
+Write-Host "Sistema rodando em http://api.autoshop.com:8080"
+Write-Host "Para a documentacao das APIs, acesse: http://api.autoshop.com:8080/docs"
+
+[void][System.Console]::ReadKey($true)
